8a1d44c115abaa9312cd0077d72ec7651ff895b7
Andersonmia2222 <mia.anderson2026@gbwl.org>
2025-10-31 12:56:52 +0000

    update

--- Main.java
+++ Main.java
@@ -1,6 +1,40 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      // BOOLEAN VARIABLES can only hold true or false 
+      boolean lightsOn = false;
+      Boolean lightsOn = false;
+      boolean dannyPresent = true; 
+
+
+   // BOOLEAN EXPRESSIONS are like questions( or comparison)
+   // where the only possible answers are true and false
+   boolean exp1 = (danny = true) // is danny here? 
+   boolean exp2 = (lightsOn ! = true) //are the lights NOT on? (off)
+   // comparing numerical values 
+
+   int x = 6;
+   int y = 7;
+
+   // check for equality using the == operator 
+   system.out.println( x== y ); // you 
+   //check for NOT equal using the != operator 
+   boolen exp3 = (x != y) //store  the boolean exp result in a veriable 
+   system.out.println( x < y); // true 
+   system.out.println( x > y); // false
+   system.out.println( x <= y); // true
+   system.out.println( x >= y); // false
+   system.out.println( x >= y); // true (ORDER MATTERS w/rel. operators)
+   System.out.println( y == y) // true 
+
+   // can you compare ints and doubles?
+   double z = 6;
+   system.out.println( x== z ); // true (6 and 6.0 are the same thing)
+
+
+
+
+
+
    }
 }
